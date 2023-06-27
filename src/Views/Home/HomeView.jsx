a747e64e5d3313b8c25cdd83b52fab6cf59a2687
Oruchan Asar <oruchan.asar@gmail.com>
2023-06-27 16:57:58 +0300

    add page banner

--- src/Views/Home/HomeView.jsx
+++ src/Views/Home/HomeView.jsx
@@ -5,41 +5,47 @@ import { getAllProducts } from "../../services/requests";
 import SwiperCard from "../../components/Swiper/SwiperCard";
 import Banner from "../../components/Banner/Banner";
 import styles from "./homeView.module.css";
-import Loading from "../../components/Loading/Loading"
+import Loading from "../../components/Loading/Loading";
+import PageBanner from "../../components/PageBanner/PageBanner";
 
 const HomeView = () => {
   const [products, setProducts] = useState([]);
   const [isLoad, setIsLoad] = useState(true);
 
-  console.log("slm")
+  console.log("slm");
 
   useEffect(() => {
-    getAllProducts().then((res) => setProducts(res)).then(()=>setIsLoad(false));
-  }
-  , []);
+    getAllProducts()
+      .then((res) => setProducts(res))
+      .then(() => setIsLoad(false));
+  }, []);
 
   return (
-  <>
+    <>
       <Banner />
       <div className={`row g-3 ${styles["page-content"]}`}>
-        {isLoad && <div className="text-center vh-100"><Loading/></div>}
-      <div className="row g-3">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-        <SwiperCard
-          cards={products
-            .sort(function (a, b) {
-              return Math.random() - 0.5;
-            })
-            .slice(0, 10)}
-          setWindow={true}
-        />
+        {isLoad && (
+          <div className="text-center vh-100">
+            <Loading />
+          </div>
+        )}
+        <div className="row g-3">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+          <PageBanner />
+          <SwiperCard
+            cards={products
+              .sort(function (a, b) {
+                return Math.random() - 0.5;
+              })
+              .slice(0, 10)}
+            setWindow={true}
+          />
         </div>
       </div>
     </>
-    )
-  
+  );
 };
 
 export default HomeView;

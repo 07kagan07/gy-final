b733e58e430194e659cbf1776a2adab00c42f80e
07kagan07 <kagancoskun32@gmail.com>
2023-06-28 02:48:48 +0300

    basket item card

--- src/components/Basket/BasketItem.jsx
+++ src/components/Basket/BasketItem.jsx
@@ -3,7 +3,26 @@ const BasketItem = ({basketItem}) => {
   
    
     console.log(basketItem)
-    return <div>{basketItem.product.title} ${basketItem.product.price*basketItem.quantity}</div>;
+    return <div className="w-100 border rounded-3 row p-2 mb-3">
+            <div className="col-lg-2 col-sm-12 ">
+                <img src={basketItem.product.image} className="img-fluid rounded-3" alt="product" />
+                <div className="d-flex my-2">
+                    <button>-</button>
+                    <input className="w-100 text-ce" type="number" value={basketItem.quantity} />
+                    <button>+</button>
+                </div>
+            </div>
+            <div className="col-lg-10 col-sm-12 d-flex flex-column justify-content-between ">
+                <h5>{basketItem.product.title}</h5>
+                <div className="d-flex justify-content-end align-items-end flex-column">
+                    <h6>${basketItem.quantity*basketItem.product.price}</h6>
+                </div>
+            </div>
+
+        </div>;
 }
 
 export default BasketItem;
+
+
+// ${basketItem.product.price*basketItem.quantity}
